9cfee15a50d461f93705b824695510fe50a25932
yudenghui1 <yudenghui1@bytedance.com>
2021-10-19 22:03:21 +0800

    1

--- ddm/src/windows/ddcom_utils.h
+++ ddm/src/windows/ddcom_utils.h
@@ -26,6 +26,25 @@ enum class com_thread_model
 HRESULT write_com_init_register(const std::wstring& clsid, const std::wstring& desc, com_thread_model threadModel, const std::wstring dllFullPath);
 HRESULT write_com_uninit_register(const std::wstring& clsid);
 
+#define  DDREF_COUNT_GEN(CALL_TYPE, REF_COUNT_DEF)                                                                     \
+    inline unsigned long CALL_TYPE Release()                                                                           \
+    {                                                                                                                  \
+        unsigned long cRef = static_cast<unsigned long>(InterlockedDecrement(reinterpret_cast<long*>(&m_RefCount)));   \
+        if (cRef == 0) {                                                                                               \
+            delete this;                                                                                               \
+        }                                                                                                              \
+        return cRef;                                                                                                   \
+    }                                                                                                                  \
+    inline unsigned long CALL_TYPE RefCount()                                                                          \
+    {                                                                                                                  \
+        return REF_COUNT_DEF;                                                                                          \
+    }                                                                                                                  \
+    inline unsigned long RefCount()                                                                                    \
+    {                                                                                                                  \
+        return m_RefCount;                                                                                             \
+    }                                                                                                                  \
+    unsigned long REF_COUNT_DEF = 0;                                                                                   \
+
 template<typename T>
 using DDComPtr = Microsoft::WRL::ComPtr<T>;
 
@@ -33,29 +52,9 @@ class DDRefCountBase
 {
 public:
     virtual ~DDRefCountBase() { }
-    inline unsigned long AddRef() throw()
-    {
-        InterlockedIncrement(reinterpret_cast<long*>(&m_RefCount));
-        return m_RefCount;
-    }
-
-    inline unsigned long Release() throw()
-    {
-        unsigned long cRef = static_cast<unsigned long>(InterlockedDecrement(reinterpret_cast<long*>(&m_RefCount)));
-        if (cRef == 0) {
-            delete this;
-        }
-
-        return cRef;
-    }
-
-    inline unsigned long RefCount() const throw()
-    {
-        return m_RefCount;
-    }
-protected:
-    unsigned long m_RefCount = 0;
+    DDREF_COUNT_GEN(__stdcall, m_RefCount);
 };
+
 
 END_NSP_DDM
 #endif
