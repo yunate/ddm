96efebfceec0a4b0a34b8a0cc65879ec8cefdaed
yudenghui1 <yudenghui1@bytedance.com>
2021-10-19 22:08:00 +0800

    1

--- ddm/src/windows/ddcom_utils.h
+++ ddm/src/windows/ddcom_utils.h
@@ -26,24 +26,25 @@ enum class com_thread_model
 HRESULT write_com_init_register(const std::wstring& clsid, const std::wstring& desc, com_thread_model threadModel, const std::wstring dllFullPath);
 HRESULT write_com_uninit_register(const std::wstring& clsid);
 
-#define  DDREF_COUNT_GEN(CALL_TYPE, REF_COUNT_DEF)                                                                     \
-    inline unsigned long CALL_TYPE Release()                                                                           \
-    {                                                                                                                  \
-        unsigned long cRef = static_cast<unsigned long>(InterlockedDecrement(reinterpret_cast<long*>(&m_RefCount)));   \
-        if (cRef == 0) {                                                                                               \
-            delete this;                                                                                               \
-        }                                                                                                              \
-        return cRef;                                                                                                   \
-    }                                                                                                                  \
-    inline unsigned long CALL_TYPE RefCount()                                                                          \
-    {                                                                                                                  \
-        return REF_COUNT_DEF;                                                                                          \
-    }                                                                                                                  \
-    inline unsigned long RefCount()                                                                                    \
-    {                                                                                                                  \
-        return m_RefCount;                                                                                             \
-    }                                                                                                                  \
-    unsigned long REF_COUNT_DEF = 0;                                                                                   \
+#define  DDREF_COUNT_GEN(CALL_TYPE, REF_COUNT_DEF)                                                                        \
+    inline unsigned long CALL_TYPE Release()                                                                              \
+    {                                                                                                                     \
+        unsigned long cRef = static_cast<unsigned long>(InterlockedDecrement(reinterpret_cast<long*>(&REF_COUNT_DEF)));   \
+        if (cRef == 0) {                                                                                                  \
+            delete this;                                                                                                  \
+        }                                                                                                                 \
+        return cRef;                                                                                                      \
+    }                                                                                                                     \
+    inline unsigned long CALL_TYPE AddRef()                                                                               \
+    {                                                                                                                     \
+        InterlockedIncrement(reinterpret_cast<long*>(&REF_COUNT_DEF));                                                    \
+        return REF_COUNT_DEF;                                                                                             \
+    }                                                                                                                     \
+    inline unsigned long CALL_TYPE RefCount()                                                                             \
+    {                                                                                                                     \
+        return REF_COUNT_DEF;                                                                                             \
+    }                                                                                                                     \
+    unsigned long REF_COUNT_DEF = 0;                                                                                      \
 
 template<typename T>
 using DDComPtr = Microsoft::WRL::ComPtr<T>;
