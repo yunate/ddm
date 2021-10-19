6480a3a0c7f2df5b5806856a83d61b6dc6afe15c
yudenghui1 <yudenghui1@bytedance.com>
2021-10-19 21:48:09 +0800

    1

---
+++ ddm/src/ddcom_ptr.h
@@ -0,0 +1,10 @@
+#ifndef ddcom_ptr_h_
+#define ddcom_ptr_h_
+#include "g_def.h"
+
+BEG_NSP_DDM
+
+
+
+END_NSP_DDM
+#endif
--- ddm/src/windows/ddcom_utils.h
+++ ddm/src/windows/ddcom_utils.h
@@ -5,6 +5,7 @@
 
 #ifdef DD_WINDOW
 #include <windows.h>
+#include <wrl/client.h>
 BEG_NSP_DDM
 
 // https://docs.microsoft.com/en-us/windows/win32/com/inprocserver32?redirectedfrom=MSDN
@@ -24,6 +25,37 @@ enum class com_thread_model
 //////////////////////////////////////////////////////////////////////////
 HRESULT write_com_init_register(const std::wstring& clsid, const std::wstring& desc, com_thread_model threadModel, const std::wstring dllFullPath);
 HRESULT write_com_uninit_register(const std::wstring& clsid);
+
+template<typename T>
+using ComPtr = Microsoft::WRL::ComPtr<T>;
+
+class RefCountBase
+{
+public:
+    virtual ~RefCountBase() { }
+    inline unsigned long AddRef() throw()
+    {
+        InterlockedIncrement(reinterpret_cast<long*>(&m_RefCount));
+        return m_RefCount;
+    }
+
+    inline unsigned long Release() throw()
+    {
+        unsigned long cRef = static_cast<unsigned long>(InterlockedDecrement(reinterpret_cast<long*>(&m_RefCount)));
+        if (cRef == 0) {
+            delete this;
+        }
+
+        return cRef;
+    }
+
+    inline unsigned long RefCount() const throw()
+    {
+        return m_RefCount;
+    }
+protected:
+    unsigned long m_RefCount = 0;
+};
 
 END_NSP_DDM
 #endif
