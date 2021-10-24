201f09f6f4c927bb0938426c55e5abc5153c3453
yunate <yunate@foxmail.com>
2021-10-24 19:35:36 +0800

    1

--- ddm/src/windows/ddwin_utils.h
+++ ddm/src/windows/ddwin_utils.h
@@ -25,19 +25,33 @@ std::string last_error_msga(DWORD errorCode);
 // eg: guidStr:"{431ECE1A-B4EB-4C15-8899-CAB52D13A06A}"
 inline bool ddstr_guid(const std::wstring& guidStr, ::GUID& guid)
 {
-    int cnt = ::swscanf_s(guidStr.c_str(), L"{%08X-%04hX-%04hX-%02hhX%02hhX-%02hhX%02hhX%02hhX%02hhX%02hhX%02hhX}",
-        &(guid.Data1), &(guid.Data2), &(guid.Data3),
-        &(guid.Data4[0]), &(guid.Data4[1]), &(guid.Data4[2]), &(guid.Data4[3]),
-        &(guid.Data4[4]), &(guid.Data4[5]), &(guid.Data4[6]), &(guid.Data4[7]));
+    short d0, d1, d2, d3, d4, d5, d6, d7;
+    int cnt = ::swscanf_s(guidStr.c_str(), L"{%08X-%04hX-%04hX-%02hX%02hX-%02hX%02hX%02hX%02hX%02hX%02hX}",
+        &(guid.Data1), &(guid.Data2), &(guid.Data3), &d0, &d1, &d2, &d3, &d4, &d5, &d6, &d7);
+    guid.Data4[0] = (unsigned char)d0;
+    guid.Data4[1] = (unsigned char)d1;
+    guid.Data4[2] = (unsigned char)d2;
+    guid.Data4[3] = (unsigned char)d3;
+    guid.Data4[4] = (unsigned char)d4;
+    guid.Data4[5] = (unsigned char)d5;
+    guid.Data4[6] = (unsigned char)d6;
+    guid.Data4[7] = (unsigned char)d7;
     return cnt == 11;
 }
 
 inline bool ddstr_guid(const std::string& guidStr, ::GUID& guid)
 {
-    int cnt = ::sscanf_s(guidStr.c_str(), "{%08X-%04hX-%04hX-%02hhX%02hhX-%02hhX%02hhX%02hhX%02hhX%02hhX%02hhX}",
-        &(guid.Data1), &(guid.Data2), &(guid.Data3),
-        &(guid.Data4[0]), &(guid.Data4[1]), &(guid.Data4[2]), &(guid.Data4[3]),
-        &(guid.Data4[4]), &(guid.Data4[5]), &(guid.Data4[6]), &(guid.Data4[7]));
+    short d0, d1, d2, d3, d4, d5, d6, d7;
+    int cnt = ::sscanf_s(guidStr.c_str(), "{%08X-%04hX-%04hX-%02hX%02hX-%02hX%02hX%02hX%02hX%02hX%02hX}",
+                          &(guid.Data1), &(guid.Data2), &(guid.Data3), &d0, &d1, &d2, &d3, &d4, &d5, &d6, &d7);
+    guid.Data4[0] = (unsigned char)d0;
+    guid.Data4[1] = (unsigned char)d1;
+    guid.Data4[2] = (unsigned char)d2;
+    guid.Data4[3] = (unsigned char)d3;
+    guid.Data4[4] = (unsigned char)d4;
+    guid.Data4[5] = (unsigned char)d5;
+    guid.Data4[6] = (unsigned char)d6;
+    guid.Data4[7] = (unsigned char)d7;
     return cnt == 11;
 }
 
--- test/src/ddm/thread/async/test_sync_n_buff.cpp
+++ test/src/ddm/thread/async/test_sync_n_buff.cpp
@@ -10,9 +10,9 @@ TEST(test_sync_n_buff, test_sync_n_buff_queue)
     xx.push(1);
     xx.push(2);
     xx.push(3);
-    bool l = xx.push(4, 10);
+    bool l = xx.push(4, 10); l;
     std::queue<int> que;
-    int yy = xx.pop();
+    int yy = xx.pop(); yy;
 }
 
 TEST(test_sync_n_buff, test_sync_n_buff_stack)
@@ -24,6 +24,6 @@ TEST(test_sync_n_buff, test_sync_n_buff_stack)
     xx.push(3);
     xx.push(4);
     std::queue<int> que;
-    int yy = xx.pop();
+    int yy = xx.pop(); yy;
 }
 END_NSP_DDM
